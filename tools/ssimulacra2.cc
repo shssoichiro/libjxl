aace7e9f9c18bbc3ebcf69a988aac50e42d7a7b5
Josh Holmer <jholmer.in@gmail.com>
2022-10-22 17:54:01 -0400

    Checking blur

--- tools/ssimulacra2.cc
+++ tools/ssimulacra2.cc
@@ -334,16 +334,16 @@ Msssim ComputeSSIMULACRA2(const jxl::ImageBundle& orig,
     blur.ShrinkTo(img1.xsize(), img2.ysize());
 
     Multiply(img1, img1, &mul);
-    for (size_t y = 0; y < mul.ysize(); ++y) {
-      const float* JXL_RESTRICT in1 = mul.PlaneRow(0, y);
-      const float* JXL_RESTRICT in2 = mul.PlaneRow(1, y);
-      const float* JXL_RESTRICT in3 = mul.PlaneRow(2, y);
-      for (size_t x = 0; x < mul.xsize(); ++x) {
+    Image3F sigma1_sq = blur(mul);
+    for (size_t y = 0; y < sigma1_sq.ysize(); ++y) {
+      const float* JXL_RESTRICT in1 = sigma1_sq.PlaneRow(0, y);
+      const float* JXL_RESTRICT in2 = sigma1_sq.PlaneRow(1, y);
+      const float* JXL_RESTRICT in3 = sigma1_sq.PlaneRow(2, y);
+      for (size_t x = 0; x < sigma1_sq.xsize(); ++x) {
         fprintf(stderr, "%.6f %.6f %.6f\n", in1[x], in2[x], in3[x]);
       }
     }
     exit(0);
-    Image3F sigma1_sq = blur(mul);
 
     Multiply(img2, img2, &mul);
     Image3F sigma2_sq = blur(mul);
